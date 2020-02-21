a1cd91e24b0c2a6e71469086d4cb2cf633660391
tashenea <tmb15@humboldt.edu>
2020-02-21 01:01:41 -0800

    Used the Professors code as a template.
    Signed-off-by: tashenea <tmb15@humboldt.edu>

---
+++ DeckOfCards/Assets/Scripts/Deck.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+
+public class Deck
+{
+    public LinkedList<Card> Cards { get; set; }
+
+    public Deck()
+    {
+        Cards = new LinkedList<Card>();
+    }
+
+    public void AddToTop(Card card)
+    {
+        Cards.AddLast(card);
+    }
+
+    public Card RemoveFromTop()
+    {
+        Card topCard = Cards.Last();
+        Cards.RemoveLast();
+        return topCard;
+
+    }
+}
+
+    //public void Shuffle()
+    //{
+
+   
